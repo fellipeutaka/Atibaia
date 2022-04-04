cc8712e4fdfd276a49b7a7d19294c4adb203cd89
Fellipe Utaka <fellipeutaka@gmail.com>
2022-04-04 20:57:51 -0300

    Home screen

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -32,15 +32,19 @@ Partial Class Form1
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
         Me.ProgressBar3 = New System.Windows.Forms.ProgressBar()
-        Me.ProgressBar4 = New System.Windows.Forms.ProgressBar()
-        Me.ProgressBar5 = New System.Windows.Forms.ProgressBar()
-        Me.ProgressBar6 = New System.Windows.Forms.ProgressBar()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(622, 101)
+        Me.Label1.Location = New System.Drawing.Point(474, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(24, 15)
         Me.Label1.TabIndex = 0
@@ -49,7 +53,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 101)
+        Me.Label2.Location = New System.Drawing.Point(11, 56)
         Me.Label2.Margin = New System.Windows.Forms.Padding(0)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(43, 15)
@@ -59,7 +63,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 131)
+        Me.Label3.Location = New System.Drawing.Point(11, 86)
         Me.Label3.Margin = New System.Windows.Forms.Padding(0)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(47, 15)
@@ -69,7 +73,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(12, 161)
+        Me.Label4.Location = New System.Drawing.Point(11, 116)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(36, 15)
         Me.Label4.TabIndex = 3
@@ -78,7 +82,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(12, 191)
+        Me.Label5.Location = New System.Drawing.Point(11, 146)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(52, 15)
         Me.Label5.TabIndex = 4
@@ -87,7 +91,7 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(12, 221)
+        Me.Label6.Location = New System.Drawing.Point(10, 206)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(44, 15)
         Me.Label6.TabIndex = 5
@@ -96,7 +100,7 @@ Partial Class Form1
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(12, 251)
+        Me.Label7.Location = New System.Drawing.Point(10, 176)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(68, 15)
         Me.Label7.TabIndex = 6
@@ -104,54 +108,93 @@ Partial Class Form1
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(138, 101)
+        Me.ProgressBar1.Location = New System.Drawing.Point(98, 56)
         Me.ProgressBar1.Name = "ProgressBar1"
         Me.ProgressBar1.Size = New System.Drawing.Size(100, 15)
         Me.ProgressBar1.TabIndex = 7
+        Me.ProgressBar1.Value = 100
         '
         'ProgressBar2
         '
-        Me.ProgressBar2.Location = New System.Drawing.Point(138, 131)
+        Me.ProgressBar2.Location = New System.Drawing.Point(98, 86)
         Me.ProgressBar2.Name = "ProgressBar2"
         Me.ProgressBar2.Size = New System.Drawing.Size(100, 15)
         Me.ProgressBar2.TabIndex = 8
         '
         'ProgressBar3
         '
-        Me.ProgressBar3.Location = New System.Drawing.Point(138, 161)
+        Me.ProgressBar3.Location = New System.Drawing.Point(98, 116)
         Me.ProgressBar3.Name = "ProgressBar3"
         Me.ProgressBar3.Size = New System.Drawing.Size(100, 15)
         Me.ProgressBar3.TabIndex = 9
         '
-        'ProgressBar4
+        'Label8
         '
-        Me.ProgressBar4.Location = New System.Drawing.Point(138, 191)
-        Me.ProgressBar4.Name = "ProgressBar4"
-        Me.ProgressBar4.Size = New System.Drawing.Size(100, 15)
-        Me.ProgressBar4.TabIndex = 10
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(98, 206)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(19, 15)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "$0"
         '
-        'ProgressBar5
+        'Label9
         '
-        Me.ProgressBar5.Location = New System.Drawing.Point(138, 221)
-        Me.ProgressBar5.Name = "ProgressBar5"
-        Me.ProgressBar5.Size = New System.Drawing.Size(100, 15)
-        Me.ProgressBar5.TabIndex = 11
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(98, 176)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(36, 15)
+        Me.Label9.TabIndex = 14
+        Me.Label9.Text = "0 / 20"
         '
-        'ProgressBar6
+        'Label10
         '
-        Me.ProgressBar6.Location = New System.Drawing.Point(138, 251)
-        Me.ProgressBar6.Name = "ProgressBar6"
-        Me.ProgressBar6.Size = New System.Drawing.Size(100, 15)
-        Me.ProgressBar6.TabIndex = 12
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(98, 146)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(36, 15)
+        Me.Label10.TabIndex = 15
+        Me.Label10.Text = "0 / 20"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Atibaia.My.Resources.Resources._me
+        Me.PictureBox1.Location = New System.Drawing.Point(474, 30)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(96, 96)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 16
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(486, 299)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 31)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Map"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label11.Location = New System.Drawing.Point(11, 9)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(121, 32)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "Your stats:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.ProgressBar6)
-        Me.Controls.Add(Me.ProgressBar5)
-        Me.Controls.Add(Me.ProgressBar4)
+        Me.ClientSize = New System.Drawing.Size(582, 342)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.ProgressBar3)
         Me.Controls.Add(Me.ProgressBar2)
         Me.Controls.Add(Me.ProgressBar1)
@@ -165,6 +208,7 @@ Partial Class Form1
         Me.Name = "Form1"
         Me.Text = "Atibaia"
         Me.TopMost = True
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -180,7 +224,10 @@ Partial Class Form1
     Friend WithEvents ProgressBar1 As ProgressBar
     Friend WithEvents ProgressBar2 As ProgressBar
     Friend WithEvents ProgressBar3 As ProgressBar
-    Friend WithEvents ProgressBar4 As ProgressBar
-    Friend WithEvents ProgressBar5 As ProgressBar
-    Friend WithEvents ProgressBar6 As ProgressBar
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label11 As Label
 End Class
